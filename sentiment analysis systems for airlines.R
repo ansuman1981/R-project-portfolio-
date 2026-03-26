eebe74ec0b269f5e071e911eeb41f713a5507d19
ansuman1981 <ansuman.jaiswal@my.jcu.edu.au>
2026-03-26 21:37:53 +0800

    get how mamy negative words

--- sentiment analysis systems for airlines.R
+++ sentiment analysis systems for airlines.R
@@ -8,11 +8,11 @@ install.packages("dplyr") # helps to manupiulate data frames
 
 library(tidytext)
 library(dplyr)
-
+#2i
 airline <- read.csv("airline_sentiment (2).csv")
 dim(airline)
 names(airline)
-
+#2ii
 ## what xg is (confidence cut-off for positive tweets).
 xg <- 0.7 # only trust positive tweets with confidence above 0.7
 
@@ -21,6 +21,8 @@ positive_row <- subset(airline,
                        airline_sentiment.confidence > xg)
 nrow(positive_row)
 #  now we decide how many good words 
+xb <- 0.7
+
 ng<- 100   #From all words in these positive tweets
 
 #%>%: the pipe; it means “take the result on the left and pass it into the next function.”
@@ -34,3 +36,7 @@ positive_words <- positive_row %>%
 good_words_count <- positive_words %>%
   count(word, sort=TRUE)
 head(good_words_count,10)
+
+#2iii
+# get how many bad words less than the xb
+xb <
