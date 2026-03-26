1fd85a1b3780a3a9dcc386cfefb994ad01460173
ansuman1981 <ansuman.jaiswal@my.jcu.edu.au>
2026-03-26 20:46:36 +0800

    display positive nrow now find good word

--- sentiment analysis systems for airlines.R
+++ sentiment analysis systems for airlines.R
@@ -13,3 +13,6 @@ xg <- 0.7 # only trust positive tweets with confidence above 0.7
 positive_row <- subset(airline,
                        airline_sentiment == "positive" &
                        airline_sentiment.confidence > xg)
+nrow(positive_row)
+#  now we decide how many good words 
+ng<- 100
