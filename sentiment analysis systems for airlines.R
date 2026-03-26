1322e8e5171f56b0dbc35b7a5040172d95d8b172
ansuman1981 <ansuman.jaiswal@my.jcu.edu.au>
2026-03-26 20:40:18 +0800

    create a row for positive tweet

--- sentiment analysis systems for airlines.R
+++ sentiment analysis systems for airlines.R
@@ -8,4 +8,8 @@ dim(airline)
 names(airline)
 
 ## what xg is (confidence cut-off for positive tweets).
-xg <- 0.7
+xg <- 0.7 # only trust positive tweets with confidence above 0.7
+
+positive_row <- subset(airline,
+                       airline_sentiment == "positive" &
+                       airline_sentiment.confidence > xg)
